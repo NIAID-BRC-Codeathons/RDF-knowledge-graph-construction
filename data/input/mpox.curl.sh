f352b4961ccd83f9cb617f71c958c7b25b34745d
davidyuyuan <davidyuan@ebi.ac.uk>
2025-11-12 21:10:09 -0600

    Adding an example to dump all metadata of viral pathogens in ENA

---
+++ data/input/mpox.curl.sh
@@ -0,0 +1,5 @@
+# Example command to retrieve all fields for the pathogen with tax ID 10244 in TSV format from the ENA portal API.
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_run&query=tax_eq(10244)&fields=ALL&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > ./mpox.tsv
+
+# Example command to retrieve all fields for all the viral pathogens in TSV format from the ENA portal API.
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_run&query=tag="pathogen:virus"&fields=ALL&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > ./mpox.tsv
